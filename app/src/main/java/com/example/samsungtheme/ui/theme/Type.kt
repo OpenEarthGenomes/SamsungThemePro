68b4088ca749f06b611b67bd3a646752953fb8f4
Fáber Sándor <sandorwow@gmail.com>
2026-01-18 18:23:42 +0100

    Update Type.kt

--- app/src/main/java/com/example/samsungtheme/ui/theme/Type.kt
+++ app/src/main/java/com/example/samsungtheme/ui/theme/Type.kt
@@ -1,12 +1,10 @@
 package com.example.samsungtheme.ui.theme
-
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
-// Define a minimal typography
 val Typography = Typography(
     bodyLarge = TextStyle(
         fontFamily = FontFamily.Default,
