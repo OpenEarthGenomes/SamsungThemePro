cfea78dc30e1d91307085fcc2f2010d41418d311
Fáber Sándor <sandorwow@gmail.com>
2026-01-18 18:31:58 +0100

    Update Color.kt

--- app/src/main/java/com/example/samsungtheme/ui/theme/Color.kt
+++ app/src/main/java/com/example/samsungtheme/ui/theme/Color.kt
@@ -1,12 +1,10 @@
 package com.example.samsungtheme.ui.theme
-
 import androidx.compose.ui.graphics.Color
 
-// Samsung A35 Color Palette
-val DeepSeaBackground = Color(0xFF101E23) // Sötét tengerkék háttér
-val MintGreenPrimary = Color(0xFF99FFB3)  // Mentazöld gombok
-val TealSecondary = Color(0xFF40E0D0)    // Türkiz elemek
-val LightCyanText = Color(0xFFE0FFFF)    // Világos szöveg
-val CardDarkBlue = Color(0xFF1A2B32)     // Kártyák sötétkéke
-val SamsungBlue = Color(0xFF0066CC)      // Samsung kék
-val SamsungPurple = Color(0xFF9933CC)    // Samsung lila
+val DeepSeaBackground = Color(0xFF101E23)
+val MintGreenPrimary = Color(0xFF99FFB3)
+val TealSecondary = Color(0xFF40E0D0)
+val LightCyanText = Color(0xFFE0FFFF)
+val CardDarkBlue = Color(0xFF1A2B32)
+val SamsungBlue = Color(0xFF0066CC)
+val SamsungPurple = Color(0xFF9933CC)
